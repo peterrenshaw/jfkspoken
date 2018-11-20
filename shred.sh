c59f6b3135f03fb104976195af44b6a3d743fa08
Peter Renshaw <peterrenshaw@seldomlogical.com>
2018-11-20 17:29:34 +1100

    2018NOV20 added shell scripts

---
+++ shred.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#=======
+# name: shred.sh
+# date: 2018NOV20
+#       2017AUG12
+# prog: pr
+# desc: simple script to avoid time wasting
+#=======
+
+{ 
+    python3 extract.py
+} >&2
---
+++ tweet.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+#=======
+# name: tweet.py
+# date: 2017AUG14
+# prog: pr
+# desc: called via crontab.cron
+#       calls eventer.py which calculates
+#       latest tweet to send
+# usag: will show up in console via cron with the below echos
+#       errors will be mailed
+#=======
+
+
+{ 
+    # production or testing?
+    TRUE=1
+    FALSE=0
+    IS_PROD=$FALSE
+
+    # paths
+    # depends on linux (production) or mac (development)
+    LIN_USER=pi
+    LIN_HOME=/home/$LI_USER/code
+   
+    MAC_USER=pr
+    MAC_HOME=/Users/$MAC_USER/work/code
+
+    CODE=pureausten
+    if [ "$IS_PROD" = "$TRUE" ]; then
+        CODE_PATH=$LIN_HOME/$CODE
+    else
+        CODE_PATH=$MAC_HOME/$CODE
+    fi
+    PYVER=python3
+
+    echo "tweet.sh start"
+    PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+    $PYVER $CODE_PATH/eventer.py -s
+    echo "eventer called"
+    echo "tweet.sh stop"
+} >&2
